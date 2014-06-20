eb28f5c88dcee68cab387c3cef1a5a5d58532faa
Danti Chen <dantichen@dantis-air.wireless.yale.internal>
2014-06-20 15:40:14 -0400

    added commited

--- simple_generate.sh
+++ simple_generate.sh
@@ -1,6 +1,8 @@
-for i in {1..20};
+ for i in {1..10};
 do
     let j=i+1
-    echo $i $j>>data1.txt
+    echo $i $j>>data1temp.txt
 done
+awk '{print $1, $2^2 }' data1temp.txt 
+cat data1temp.txt>data1.txt
 
